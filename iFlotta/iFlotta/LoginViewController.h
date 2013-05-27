b65e55166a2bb1b6a2f111f43dc7e2c6c8d37ae2
RexpecT <itothimre@gmail.com>
2013-05-27 13:57:33 +0200

    splash-t kipofoztam, gyonyorkodjetek :D

---
+++ iFlotta/iFlotta/LoginViewController.h
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.h
+//  iFlotta
+//
+//  Created by Csüti on 5/9/13.
+//  Copyright (c) 2013 Csüti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *userName;
+@property (weak, nonatomic) IBOutlet UITextField *userPassword;
+@property (strong, nonatomic) IBOutlet UIButton *loginButton;
+@property (strong, nonatomic) IBOutlet UILabel *userNameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *userPasswordLabel;
+
+@property (strong, nonatomic) IBOutlet UIImageView *logo;
+@property (strong, nonatomic) IBOutlet UIImageView *pic1;
+@property (strong, nonatomic) IBOutlet UIImageView *pic2;
+@property (strong, nonatomic) IBOutlet UIImageView *pic3;
+@property (strong, nonatomic) IBOutlet UIImageView *pic4;
+
+- (IBAction)userLoginButton:(id)sender;
+
+@end
---
+++ iFlotta/iFlotta/LoginViewController.m
@@ -0,0 +1,144 @@
+//
+//  LoginViewController.m
+//  iFlotta
+//
+//  Created by Csüti on 5/9/13.
+//  Copyright (c) 2013 Csüti. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "AppDelegate.h"
+#import "Sofor.h"
+#import "DataBaseUtil.h"
+#import "JsonUtil.h"
+#import "MainPageNavigationViewController.h"
+
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+@interface LoginViewController (){
+    NSArray* _imgPicsArray;
+    int idx;
+}
+
+
+
+@end
+
+@implementation LoginViewController
+@synthesize pic1;
+@synthesize pic2;
+@synthesize pic3;
+@synthesize pic4;
+@synthesize loginButton;
+@synthesize userName;
+@synthesize userNameLabel;
+@synthesize userPassword;
+@synthesize userPasswordLabel;
+
+
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        
+    }
+    
+    
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    loginButton.hidden = true;
+    userName.hidden = true;
+    userNameLabel.hidden =true;
+    userPassword.hidden = true;
+    userPasswordLabel.hidden = true;
+    self.view.backgroundColor = UIColorFromRGB(0xA6977C);
+    idx = 0;
+    _imgPicsArray = [NSArray arrayWithObjects:@"auto-ikon.png", @"telephely-ikon.png", @"partnerek_ikon.png", @"munka-ikon2.png", nil];
+    for (int i = 0; i < _imgPicsArray.count; i++) {
+        [NSTimer scheduledTimerWithTimeInterval:(i+1)*0.6 target:self selector:@selector(addPic:) userInfo:nil repeats:NO];
+    }
+    
+    [NSTimer scheduledTimerWithTimeInterval:3.5 target:self selector:@selector(changeToLogin) userInfo:nil repeats:NO];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// Login
+- (IBAction)userLoginButton:(id)sender {
+    
+    NSArray *fetchedObjects = [DataBaseUtil fetchRequest:@"Sofor" :@"1" :@"soforIsActive"];
+    
+    for (NSManagedObject *proba in fetchedObjects) {
+        NSString* tmpLogin = [proba valueForKey:@"soforLogin"];
+        NSString* tmpPassword = [proba valueForKey:@"soforPass"];
+        if ([tmpLogin isEqualToString:self.userName.text] && [tmpPassword isEqualToString:self.userPassword.text] )
+        {
+            // ide majd a navigációt kell beilleszteni a következő page-re
+            MainPageNavigationViewController *mainPage = [[UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil] instantiateViewControllerWithIdentifier:@"MainPageStoryboardID"];
+        
+        [self presentViewController:mainPage animated:YES completion:^{
+            
+        }];
+        }
+
+    }
+}
+
+-(void)changeToLogin{
+    pic1.hidden = true;
+    pic2.hidden = true;
+    pic3.hidden = true;
+    pic4.hidden = true;
+    loginButton.hidden = false;
+    userName.hidden = false;
+    userNameLabel.hidden =false;
+    userPassword.hidden = false;
+    userPasswordLabel.hidden = false;
+
+}
+
+-(void)addPic:(id)sender{
+    switch (idx) {
+        case 0:
+            pic1 = [[UIImageView alloc] initWithFrame:CGRectMake(22.5+70*idx, 550/3+5 , 60, 60)];
+            pic1.image = [UIImage imageNamed:[_imgPicsArray objectAtIndex:idx]];
+            
+            [self.view addSubview:pic1];
+            break;
+        case 1:
+            pic2 = [[UIImageView alloc] initWithFrame:CGRectMake(22.5+70*idx, 550/3+5 , 60, 60)];
+            pic2.image = [UIImage imageNamed:[_imgPicsArray objectAtIndex:idx]];
+            [self.view addSubview:pic2];
+            
+            break;
+        case 2:
+            pic3 = [[UIImageView alloc] initWithFrame:CGRectMake(22.5+70*idx, 550/3+5, 60, 60)];
+            pic3.image = [UIImage imageNamed:[_imgPicsArray objectAtIndex:idx]];
+            [self.view addSubview:pic3];
+            
+            break;
+        case 3:
+            pic4 = [[UIImageView alloc] initWithFrame:CGRectMake(22.5+70*idx, 550/3+5, 60, 60)];
+            pic4.image = [UIImage imageNamed:[_imgPicsArray objectAtIndex:idx]];
+            [self.view addSubview:pic4];
+            
+            break;
+        default:
+            break;
+    }
+    idx++;
+}
+
+@end
