562665b357f2c5941a753854ca65a2109147f89b
RexpecT <itothimre@gmail.com>
2013-05-29 00:32:14 +0200

    storyboard mondhatni vegleges

---
+++ iFlotta/iFlotta/VehiclesDetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  VehiclesDetailsViewController.h
+//  iFlott
+//
+//  Created by hallgato1 on 2013.05.29..
+//  Copyright (c) 2013 Csüti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VehiclesDetailsViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *vehicleType;
+@property (strong, nonatomic) IBOutlet UILabel *speedometer;
+@property (strong, nonatomic) IBOutlet UILabel *fuel;
+
+@end
---
+++ iFlotta/iFlotta/VehiclesDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  VehiclesDetailsViewController.m
+//  iFlott
+//
+//  Created by hallgato1 on 2013.05.29..
+//  Copyright (c) 2013 Csüti. All rights reserved.
+//
+
+#import "VehiclesDetailsViewController.h"
+
+@interface VehiclesDetailsViewController ()
+
+@end
+
+@implementation VehiclesDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
