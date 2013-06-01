e224f0fe5f1c5cda06b21da4b4113e1c8091981c
hallgato5 <hallgato5@hallgato5.local>
2013-06-01 21:22:26 +0200

    MapKit ellezdve

---
+++ iFlotta/iFlotta/MapPinAnnotation.m
@@ -0,0 +1,32 @@
+//
+//  MapPinAnnotation.m
+//  iFlott
+//
+//  Created by  hallgato5 on 2013.06.01..
+//  Copyright (c) 2013 Csüti. All rights reserved.
+//
+
+#import "MapPinAnnotation.h"
+
+@implementation MapPinAnnotation
+
+@synthesize coordinate;
+@synthesize title;
+@synthesize subtitle;
+
+- (id)initWithCoordinates:(CLLocationCoordinate2D)location
+                placeName:(NSString *)placeName
+              description:(NSString *)description;
+{
+    self = [super init];
+    if (self)
+    {
+        coordinate = location;
+        title = placeName;
+        subtitle = description;
+    }
+    
+    return self;
+}
+
+@end
